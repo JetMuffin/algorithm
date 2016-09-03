6327ac9df85fbb42d14d70d2968348240b77dd2c
JetMuffin <564936642@qq.com>
2016-09-03 19:46:54 +0800

    2016-09-03

---
+++ leetcode/383.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+    }
+};
+int main() {
+	Solution s;
+	cout<<s.canConstruct("aab", "accabaabccd");
+}
