553307caf9aceb54f9e7eeeaf5601b0ea7f1830e
Jie Chen <chenjie_sx@qiyi.com>
2017-12-07 14:56:55 +0800

    2017-12-07

--- hdu/2167.cpp
+++ hdu/2167.cpp
@@ -7,22 +7,30 @@ using namespace std;
 const int maxn = 20;
 char s[10000];
 int g[maxn][maxn];
+int sum[20000][maxn];
 int dp[20000][maxn];
-vector<int> state;
-int cal(int s, int i) {
-	int sum = 0, t = 0;
-	while(s) {
-		if(s & 1) {
-			sum += g[i][t];
-		}
-		t++;
-		s >>= 1;
+int state[20000];
+int n, cnt;
+void init() {
+	memset(dp, 0, sizeof(dp));
+	memset(state, 0, sizeof(state));
+	memset(sum, 0, sizeof(sum));
+	cnt = 0;
+	for(int i = 0; i < (1<<n); i++) {
+		if((i & (i<<1)) == 0)
+			state[cnt++] = i;
 	}
-	return sum;
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < cnt; j++) {
+			for(int k = 0; k < n; k++) {
+				if((1<<k) & state[j]) {
+					sum[j][i] += g[i][k];
+				}
+			}
+		}
+	}
 }
 int main() {
-	int n = 0;
-	memset(dp, 0, sizeof(dp));
 	while(gets(s)) {
 		if(strlen(s) == 0) continue;
 		stringstream ss(s);
@@ -34,38 +42,28 @@ int main() {
 				scanf("%d", &g[i][j]);
 			}
 		}
-		// for(int i = 0; i< n; i++) {
-		// 	for(int j = 0; j< n;j++) {
-		// 		printf("%d ", g[i][j]);
-		// 	}
-		// 	printf("\n");
-		// }
 
-		for(int i = 0; i < (1 << n); i++) {
-			if((i & (i<<1)) == 0)
-				state.push_back(i);
-		}
-		int m = state.size();
+		init();
 
-		for(int i = 0; i < m; i++) {
-			dp[i][0] = cal(state[i], 0);
+		for(int i = 0; i < cnt; i++) {
+			dp[i][0] = sum[i][0];
 		}
 
 		int sj, sk;
 		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < m; j++) {
-				for(int k = 0; k < m; k++) {
+			for(int j = 0; j < cnt; j++) {
+				for(int k = 0; k < cnt; k++) {
 					sj = state[j];
 					sk = state[k];
 					if((sj&sk) || (sj & (sk<<1)) || ((sk>>1)&sj))
 						continue;
-					dp[j][i] = max(dp[j][i], dp[k][i-1]+cal(sj, i));
+					dp[j][i] = max(dp[j][i], dp[k][i-1]+sum[j][i]);
 				}
 			}
 		}
 
 		int ans = 0;
-		for(int i = 0; i < m; i++) {
+		for(int i = 0; i < cnt; i++) {
 			ans = max(dp[i][n-1], ans);
 		}
 		printf("%d\n", ans);
